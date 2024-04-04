db3fb70bdfffa441855002ab8dfd271ac4905386
yomma <furbolg174@gmail.com>
2024-04-04 08:15:48 +0500

    init

---
+++ sb-sharp-u7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sb_sharp_u7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
